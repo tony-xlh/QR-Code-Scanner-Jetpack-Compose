f543cce8bff40ff1f022ed78eef9b57cf432317f
xulihang <xulihanghai@163.com>
2023-06-28 16:45:06 +0800

    remove unused imports

--- app/src/main/java/com/dynamsoft/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/dynamsoft/qrcodescanner/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.dynamsoft.qrcodescanner
 
-import android.R
 import android.content.Context
 import android.content.ContextWrapper
 import android.os.Bundle
@@ -10,10 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.dynamsoft.dce.CameraEnhancer
 import com.dynamsoft.dce.DCECameraView
