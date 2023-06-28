b8b5278fd8ac1eb56bef280c8f393a3e369d4d44
xulihang <xulihanghai@163.com>
2023-06-28 17:04:45 +0800

    request camera permission

--- app/src/main/java/com/dynamsoft/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/dynamsoft/qrcodescanner/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.dynamsoft.qrcodescanner
 
+import android.Manifest
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.dynamsoft.dce.CameraEnhancer
 import com.dynamsoft.dce.DCECameraView
 import com.dynamsoft.qrcodescanner.ui.theme.QRCodeScannerTheme
@@ -20,8 +27,30 @@ class MainActivity : ComponentActivity() {
     private lateinit var mCameraEnhancer: CameraEnhancer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             QRCodeScannerTheme {
+                val context = LocalContext.current
+                var hasCameraPermission by remember {
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                }
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { granted ->
+                        hasCameraPermission = granted
+                        if (granted == true) {
+                            mCameraEnhancer.open()
+                        }
+                    }
+                )
+                LaunchedEffect(key1 = true){
+                  launcher.launch(Manifest.permission.CAMERA)
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Column(
@@ -32,9 +61,7 @@ class MainActivity : ComponentActivity() {
                             val mCameraView: DCECameraView
                             mCameraView = DCECameraView(context)
                             mCameraEnhancer.cameraView = mCameraView
-                            mCameraEnhancer.open()
                             mCameraView
-
                         })
                     }
                 }
